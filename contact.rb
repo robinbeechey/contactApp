851577261fdc8bc5702e71b1ccfb9aef1c811c71
vagrant <vagrant@lighthouse-labs>
2015-06-09 23:44:15 +0000

    refactoring

--- contact.rb
+++ contact.rb
@@ -1,171 +1,100 @@
 
 require 'pg'
+require 'colorize'
 
 class Contact 
 
   attr_accessor :first_name, :last_name, :email, :phone_number
   attr_reader :id
 
-
-  def initialize(first_name, last_name, email, phone_number, id=nil)
+  def initialize(first_name, last_name, email, id=nil)
       @first_name = first_name
       @last_name = last_name
       @email = email
-      @phone_number = phone_number
+      # @phone_number = phone_number
       @id = id
   end
 
    
   def to_s
-    # TODO: return string representation of Contact
-    "ID:#{id} Full Name: #{@first_name} #{@last_name}, E-mail: #{@email}, Phone# #{@phone_number}"
-
+    "ID:#{id} Full Name: #{@first_name} #{@last_name}, E-mail: #{@email}, Phone# #{@phone_number}".green
   end
+
 
   def is_new?
     @id.nil?
   end
 
-  # def save
-
-  #   if is_new?
-  #   result = @@con.exec_params('INSERT INTO contacts_table (first_name, last_name, email, phone_number) VALUES ($1, $2, $3, $4) returning id', [@first_name, @last_name, @email, @phone_number])
-  #     @id = result[0]['id']
-  #   else
-  #      @@con.exec_params('UPDATE contacts_table SET first_name = $1, last_name = $2, email = $3, phone_number = $4 WHERE id = $5', [@first_name, @last_name, @email, @phone_number, @id])
-  #   end
-
-  # end
-
-  def destroy
-    @@con.exec_params('DELETE FROM contacts_table WHERE id = $1', [@id])
-  end
 
   def save
-    result = $conn.exec_params('INSERT INTO contacts (first_name, last_name, email, phone_number) VALUES ($1, $2, $3, $4) returning id', [first_name, last_name, email, phone_number])
-      id = result[0]['id']
+    if is_new?
+      result = $conn.exec_params('INSERT INTO contacts (first_name, last_name, email) VALUES ($1, $2, $3)', [first_name, last_name, email])
+    else
+      $conn.exec_params('UPDATE contacts SET (first_name, last_name, email) VALUES ($1, $2, $3) WHERE id = $4', [first_name, last_name, email, id])
+    end
   end
 
- 
 
   class << self
 
-    def create(first_name, last_name, email, phone_number)
-        new_contact = Contact.new(first_name, last_name, email, phone_number)
-        new_contact.save
+    def create(first_name, last_name, email)
+      new_contact = Contact.new(first_name, last_name, email)
+      new_contact.save
+      new_contact
+    end
+
+    def delete(id)
+      $conn.exec_params('DELETE FROM contacts WHERE id = $1', [id])
+    end
+
+    def update_contact(first_name, last_name, email, id)
+      new_contact = Contact.new(first_name, last_name, email, id)
+      new_contact.save
+      new_contact
     end
 
 
-    def find_all_by_number(phone_number)
-
-      Contact.connection
-      result = nil
-        @@con.exec_params('SELECT * FROM contacts_table WHERE phone_number = $1', [phone_number]) do |rows|
-            rows.each do |row|
-              result = Contact.new(
-                  row['first_name'],
-                  row['last_name'],
-                  row['email'],
-                  row['phone_number'],
-                  row['id']
-              )
-            end
-          end
-      puts result
-
-    end
-      
-    def find_all_by_firstname(first_name)
-
-      Contact.connection
-      result = nil
-        @@con.exec_params('SELECT * FROM contacts_table WHERE first_name = $1', [first_name]) do |rows|
-            rows.each do |row|
-              result = Contact.new(
-                  row['first_name'],
-                  row['last_name'],
-                  row['email'],
-                  row['phone_number'],
-                  row['id']
-              )
-            end
-          end
-      puts result
-
+    def find_by_number(phone_number)
+      result = $conn.exec_params('SELECT * FROM contacts WHERE phone_number = $1',[phone_number])
+      create_array(result)
     end
 
 
-    def find_all_by_lastname(last_name)
-
-      Contact.connection
-      result = nil
-        @@con.exec_params('SELECT * FROM contacts_table WHERE last_name = $1', [last_name]) do |rows|
-            rows.each do |row|
-              result = Contact.new(
-                  row['first_name'],
-                  row['last_name'],
-                  row['email'],
-                  row['phone_number'],
-                  row['id']
-              )
-            end
-          end
-      puts result
-
+    def find_by_last_name(last_name)
+      result = $conn.exec_params('SELECT * FROM contacts WHERE last_name = $1',[last_name] )
+      create_array(result)
     end
 
 
     def find_by_email(email)
-
-      Contact.connection
-      result = nil
-        @@con.exec_params('SELECT * FROM contacts_table WHERE email = $1', [email]) do |rows|
-            rows.each do |row|
-              result = Contact.new(
-                  row['first_name'],
-                  row['last_name'],
-                  row['email'],
-                  row['phone_number'],
-                  row['id']
-              )
-            end
-          end
-      puts result
-
+      result = $conn.exec_params('SELECT * FROM contacts WHERE email = $1',[email] )
+      create_array(result)
     end
 
 
+    def find_by_first_name(name)
+      result = $conn.exec_params('SELECT * FROM contacts WHERE first_name = $1',[name] )
+      create_array(result)
+    end
+
+
+    def find_by_id(id)
+      result = $conn.exec_params('SELECT * FROM contacts WHERE id = $1',[id] )
+      create_array(result)
+    end
 
  
-    def list 
+    def list_all 
       result = $conn.exec_params('SELECT * FROM contacts')
       create_array(result)
     end
-    
 
-    
-    # def show(id)
-    #   Contact.connection
-    #   result = nil
-    #     @@con.exec_params('SELECT * FROM contacts_table WHERE id = $1 LIMIT 1', [id]) do |rows|
-    #         rows.each do |row|
-    #           result = Contact.new(
-    #               row['first_name'],
-    #               row['last_name'],
-    #               row['email'],
-    #               row['phone_number'],
-    #               row['id']
-    #           )
-    #         end
-    #       end
-    #       puts result
-    # end
-
+    private
 
     def create_array(contacts)
       result = []
       contacts.each do |row|
-        result << Contact.new(row['first_name'],row['last_name'],row['email'],row['phone_number'],row['id'])
+        result << Contact.new(row['first_name'],row['last_name'],row['email'],row['id'])
       end
       result
     end
--- contact_list.rb
+++ contact_list.rb
@@ -2,100 +2,140 @@ require_relative 'contact_database'
 require_relative 'contact'
 
 
-ContactDatabase
+
+def starter
 
   puts "----------Contact List----------"
-
-
-
-  def new_contact
-    puts "Enter name"
-    first_name = gets.chomp
-    puts "Enter last name"
-    last_name = gets.chomp
-    puts "Enter email"
-    email = gets.chomp
-    puts "Enter phone number"
-    phone_number = gets.chomp
-
-    Contact.create(first_name, last_name, email , phone_number)
-  end
-
-  def show
-
-  end
-
-  def list_all
-    Contact.list.each do |contact|
-      puts contact
-    end
-  end
-
-  def find
-
-  end
-
 
   loop do
 
   print %q(
 Menu
 
-new - create a new contact
-list - show all contacts
-search - find an contact by last name
-update - update contact
-find - find by, firstname, lastname, etc.
-quit - quit program
+New - create a new contact
+Update - update contact
+Delete - delete contact
+List - show all contacts
+Find - find category
+Quit - quit program
+  > ) 
+  command = gets.chomp.downcase
+    case command
+      when 'new'
+        new_contact
+      when 'update'
+        updater
+      when 'delete'
+        delete_contact
+      when 'list'
+        list 
+      when 'find'
+        find_case
+      when 'quit'
+        puts "----------The End-------------"
+        break
+    end
+
+  end
+
+end
+
+  def updater
+    print "Enter ID: "
+    @id = gets.chomp.to_i
+    if check_id?
+    # binding.pry
+      print "Enter first name: "
+      first_name = gets.chomp
+      print "Enter last name: "
+      last_name = gets.chomp
+      print "Enter email: "
+      email = gets.chomp
+      Contact.update_contact(first_name, last_name, email, @id)
+    else
+      puts "Contact not in database"
+    end
+  end
+
+  def check_id?
+    contact = Contact.find_by_id(@id)
+  end
+
+
+  def new_contact
+    print "Enter first name: "
+    first_name = gets.chomp
+    print "Enter last name: "
+    last_name = gets.chomp
+    print "Enter email: "
+    email = gets.chomp
+    # print "Enter phone number: "
+    # phone_number = gets.chomp
+
+    new_contact = Contact.create(first_name, last_name, email)
+    puts new_contact
+  end
+
+
+  def list
+    Contact.list_all.each do |contact|
+      puts contact
+    end
+  end
+
+  def delete_contact
+    print "Enter ID: "
+    input = gets.chomp.to_i
+    Contact.delete(input)
+  end
+
+  def find_prompt
+      print %q(
+  First name
+  Last name
+  Email
+  Phone
+  ID - 
   > )
-  command = gets.chomp
-
-  case command
-    when 'new'
-      new_contact
-    when 'list'
-      list_all 
-    # when 'find' 
-
-    #   puts "Select which way you want to find"
-    #   puts "first name, last name, email, phone number or delete"
-
-    #   @find = STDIN.gets.chomp
-      
-    #   if @find == 'first name'
-    #     puts "please enter name"
-    #     @name = STDIN.gets.chomp
-    #     Contact.find_all_by_firstname(@name)
-    #   elsif @find == 'last name'
-    #     puts "please enter last name"
-    #     @last = STDIN.gets.chomp
-    #     Contact.find_all_by_lastname(@last)
-    #   elsif @find == 'phone number'
-    #     puts "please enter phone number"
-    #     @number = STDIN.gets.chomp
-    #     Contact.find_all_by_number(@number)
-    #   elsif @find == 'email'
-    #     puts "please enter email"
-    #     @email = STDIN.gets.chomp
-    #     Contact.find_by_email(@email)
-    #   else      
-    #     puts "not sure what you mean by that"
-    #   end     
-
-
-    # when 'show'
-
-    #   puts "Enter ID"
-    #   id = STDIN.gets.chomp 
-    #   Contact.show(id)
-
-    
-    when 'quit'
-      puts "----------The End-------------"
-      break
+    print "Find by: "
+    @category = gets.chomp.downcase
   end
 
+  def find_case
+    find_prompt
+    case @category
+     when 'first name'
+      find_input
+      contact = Contact.find_by_first_name(@input)
+      puts contact
+    when 'last name'
+      find_input
+      contact = Contact.find_by_last_name(@input)
+      puts contact
+    when 'email'
+      find_input
+      contact = Contact.find_by_email(@input)
+      puts contact
+    when 'number'
+      find_input
+      contact = Contact.find_by_number(@input)
+      puts contact
+    when 'id'
+      find_input
+      contact = Contact.find_by_id(@input.to_i)
+      puts contact
+    end
   end
+
+  def find_input
+    print "Enter query: "
+    @input = gets.chomp
+  end
+
+
+
+
+starter
 
 
 
